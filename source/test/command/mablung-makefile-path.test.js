25f4b19e4b4a82591642fd8d56d53078e4ce5712
virtualpatterns.com <code@virtualpatterns.com>
2021-07-04 05:12:51 -0400

    stuff

--- source/test/command/mablung-makefile-path.test.js
+++
@@ -1,8 +0,0 @@
-import Test from 'ava'
-
-import { MablungMakefilePathProcess } from './mablung-makefile-path-process.js'
-
-Test('mablung-makefile-path', async (test) => {
-  let process = new MablungMakefilePathProcess()
-  test.is(await process.whenExit(), 0)
-})
