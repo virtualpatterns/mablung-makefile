3be64ab98e3332bb38f3b5113cdd49e5d99b3d97
virtualpatterns.com <code@virtualpatterns.com>
2021-07-19 23:03:02 -0400

    trying out shared babel/eslint configs

---
+++ source/library/babel-configuration.cjs
@@ -0,0 +1,9 @@
+import { createRequire as CreateRequire } from 'module'
+import FileSystem from 'fs-extra'
+import JSON5 from 'json5'
+
+const Require = CreateRequire(import.meta.url)
+
+const Package = JSON5.parse(FileSystem.readFileSync(Require.resolve('../../package.json')), { 'encoding': 'utf-8' })
+
+export default Package.babel
