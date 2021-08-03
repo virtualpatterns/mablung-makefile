bdefe775b415b5f739953f9b7fadb3a18723fd83
virtualpatterns.com <code@virtualpatterns.com>
2021-08-02 22:17:00 -0400

    removing unec stuff

--- source/header/package.js
+++ source/header/package.js
@@ -1,9 +1,5 @@
-import { createRequire as CreateRequire } from 'module'
 import FileSystem from 'fs-extra'
 
-const Process = process
-const Require = CreateRequire(import.meta.url)
-
-const Package = FileSystem.readJsonSync(Require.resolve(`${Process.cwd()}/package.json`), { 'encoding': 'utf-8' })
+const Package = FileSystem.readJsonSync('package.json', { 'encoding': 'utf-8' })
 
 export { Package }
