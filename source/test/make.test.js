a4681833a3cf62215a306c28065b7735c6f788a4
virtualpatterns.com <code@virtualpatterns.com>
2021-08-09 22:14:59 -0400

    make tests

--- source/test/make.test.js
+++ source/test/make.test.js
@@ -3,155 +3,183 @@ import Test from 'ava'
 
 import { Package } from '../library/package.js'
 
-Test.skip('(default)', (test) => {
+Test.only('null', (test) => {
+  test.is(Shell.exec('make null', { 'silent': true }).code, 2)
+})
+
+Test.only('null --just-print', (test) => {
+  test.is(Shell.exec('make null --just-print', { 'silent': true }).code, 2)
+})
+
+Test.only('(default)', (test) => {
 
   let result = Shell.exec('make', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, `${Package.name}@${Package.version}\n`)
+  test.true(stdout.includes(`${Package.name}@${Package.version}`))
 
 })
 
-Test.skip('version', (test) => {
+Test.only('version', (test) => {
 
   let result = Shell.exec('make version', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, `${Package.name}@${Package.version}\n`)
+  test.true(stdout.includes(`${Package.name}@${Package.version}`))
 
 })
 
-Test.skip('install', (test) => {
+Test.only('install --just-print', (test) => {
 
   let result = Shell.exec('make install --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, 'npm install\n')
+  test.true(stdout.includes('npm install'))
   
 })
 
-Test.skip('re-install', (test) => {
+Test.only('re-install --just-print', (test) => {
 
   let result = Shell.exec('make re-install --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout,  'rm -Rf node_modules package-lock.json\n' +
-                          'npm install\n')
+
+  test.true(stdout.includes('rm -Rf node_modules package-lock.json'))
+  test.true(stdout.includes('npm install'))
   
 })
 
-Test.skip('clean', (test) => {
+Test.only('clean --just-print', (test) => {
 
   let result = Shell.exec('make clean --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, 'npx shx rm -Rf coverage process release\n')
+  test.true(stdout.includes('npx shx rm -Rf coverage process release'))
 
 })
 
-Test.skip('run', (test) => {
+Test.only('run', (test) => {
 
   let result = Shell.exec('make run', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 2)
-  test.is(result.stdout, 'An argument must be specified (e.g. argument=release/sandbox/index.js)\n')
+  test.true(stdout.includes('An argument must be specified (e.g. argument=release/sandbox/index.js)'))
 
 })
 
-Test.skip('run argument="..."', (test) => {
+Test.only('run argument="..."', (test) => {
 
   let result = Shell.exec('make run argument="release/command/mablung-makefile.js get-version"', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, `${Package.name}@${Package.version}\n`)
+  test.true(stdout.includes(`${Package.name}@${Package.version}`))
 
 })
 
 
-Test.skip('run arg="..."', (test) => {
+Test.only('run arg="..."', (test) => {
 
   let result = Shell.exec('make run arg="release/command/mablung-makefile.js get-version"', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, `${Package.name}@${Package.version}\n`)
+  test.true(stdout.includes(`${Package.name}@${Package.version}`))
 
 })
 
-Test.skip('run a="..."', (test) => {
+Test.only('run a="..."', (test) => {
 
   let result = Shell.exec('make run a="release/command/mablung-makefile.js get-version"', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout, `${Package.name}@${Package.version}\n`)
+  test.true(stdout.includes(`${Package.name}@${Package.version}`))
 
 })
 
-Test.skip('cover', (test) => {
+Test.only('cover --just-print', (test) => {
 
   let result = Shell.exec('make cover --just-print', { 'silent': true })
   let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(stdout[stdout.length - 2], 'npx shx mv coverage ../Shared/mablung-makefile')
+  test.true(stdout.includes('npx shx mv coverage ../Shared/mablung-makefile'))
 
 })
 
-Test.skip('test', (test) => {
+Test.only('test --just-print', (test) => {
 
   let result = Shell.exec('make test --just-print', { 'silent': true })
   let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(stdout[stdout.length - 2], 'npx ava ')
+  test.true(stdout.includes('npx ava '))
 
 })
 
-Test.skip('release', (test) => {
+Test.only('release --just-print', (test) => {
 
   let result = Shell.exec('make release --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout,  'A version must be specified (e.g. version=prerelease, version=patch, or version=1.0.0)\n' +
-                          'npx shx false\n')
+  test.true(stdout.includes('A version must be specified (e.g. version=prerelease, version=patch, or version=1.0.0)'))
 
 })
 
-Test.skip('release version=...', (test) => {
+Test.only('release version=... --just-print', (test) => {
 
   let result = Shell.exec('make release version=prerelease --just-print', { 'silent': true })
   let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(stdout[stdout.length - 2], 'git push origin master')
+  test.true(stdout.includes('git push origin master'))
 
 })
 
-Test.skip('release ver=...', (test) => {
+Test.only('release ver=... --just-print', (test) => {
 
   let result = Shell.exec('make release ver=prerelease --just-print', { 'silent': true })
   let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(stdout[stdout.length - 2], 'git push origin master')
+  test.true(stdout.includes('git push origin master'))
 
 })
 
-Test.skip('release v=...', (test) => {
+Test.only('release v=... --just-print', (test) => {
 
   let result = Shell.exec('make release v=prerelease --just-print', { 'silent': true })
   let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(stdout[stdout.length - 2], 'git push origin master')
+  test.true(stdout.includes('git push origin master'))
 
 })
 
-Test.only('commit', (test) => {
+Test.only('commit --just-print', (test) => {
 
   let result = Shell.exec('make commit --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
 
   test.is(result.code, 0)
-  test.is(result.stdout,  'Git working directory clean.\n' +
-                          'npx shx true\n')
+  test.true(stdout.includes('Git working directory clean.'))
+
+})
+
+Test.only('update --just-print', (test) => {
+
+  let result = Shell.exec('make update --just-print', { 'silent': true })
+  let stdout = result.stdout.split('\n')
+
+  test.is(result.code, 0)
+  test.true(stdout.includes('npx npm-check-updates --upgrade'))
 
 })
