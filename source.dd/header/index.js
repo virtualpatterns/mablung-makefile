1c00003103935a1158ffecc4cfbf4e85bb79b64d
Frank G. Ficnar <fficnar@BEEDLE.local>
2021-08-21 03:59:17 +0000

    ugh

---
+++ source.dd/header/index.js
@@ -0,0 +1,22 @@
+import './library/source-map-support.js'
+
+import { Package } from './library/package.js'
+
+process.exitCode = 0
+
+try {
+  
+  console.log()
+  console.log(`Package:     ${Package.name}`)
+  console.log(`Description: ${Package.description}`)
+  console.log(`Version:     ${Package.version}`)
+  console.log(`License:     ${Package.license}`)
+  console.log(`Author:      ${Package.author}`)
+  console.log(`Repository:  ${Package.repository.url}`)
+  console.log(`Source:      ${process.env.SOURCE_PATH}`)
+
+/* c8 ignore next 4 */
+} catch (error) {
+  console.error(error)
+  process.exitCode = 1
+}
