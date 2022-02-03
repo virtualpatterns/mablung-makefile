f4e0b474c009b4d4ab5f7ed02303a8f65372b2b0
virtualpatterns.com <code@virtualpatterns.com>
2022-02-02 20:24:24 -0500

    updates

--- source/test/index-copy.test.js
+++ source/test/index-copy.test.js
@@ -5,24 +5,8 @@ import URL from 'url'
 
 const FolderPath = Path.dirname(URL.fileURLToPath(import.meta.url))
 
-Test('./resource/copy/folder/index.js', async (test) => {
-  test.true(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
-})
-
 Test('./resource/copy/folder/index.json', async (test) => {
   test.true((await FileSystem.readJson(Path.resolve(FolderPath, test.title), { 'encoding': 'utf-8' })).OK)
-})
-
-Test('./resource/copy/.babelrc.json', async (test) => {
-  test.true(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
-})
-
-Test('./resource/copy/.eslintrc.json', async (test) => {
-  test.true(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
-})
-
-Test('./resource/copy/index.js', async (test) => {
-  test.true(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
 })
 
 Test('./resource/copy/index.json', async (test) => {
